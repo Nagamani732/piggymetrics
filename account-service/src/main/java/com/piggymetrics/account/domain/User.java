1f204aa68d81ebc399f0a0522612a01efb64a14b
Nagamani Nagaraj <nagamani.nagaraj@oracle.com>
2023-12-08 13:14:46 +0530

    Add user creation and error handler

--- account-service/src/main/java/com/piggymetrics/account/domain/User.java
+++ account-service/src/main/java/com/piggymetrics/account/domain/User.java
@@ -3,10 +3,14 @@ package com.piggymetrics.account.domain;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document(collection = "users")
 public class User {
 
+    @Id
     @NotNull
     @Length(min = 3, max = 20)
     private String username;
